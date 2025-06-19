3d388f80dcdad69cd5906ddc5f3395e34fdac349
Mark Heinonen <heinonenmh@gmail.com>
2025-06-18 22:49:35 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 /* --------  service-worker.js  -------- */
 
-const CACHE_NAME   = "rook-v1.4.504";      //  ←  bump on every release
+const CACHE_NAME   = "rook-v1.4.505";      //  ←  bump on every release
 const CORE_ASSETS  = [
   "/",                       // alias for index.html when online
   "/index.html",             // app shell
